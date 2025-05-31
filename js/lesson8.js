37986021c84b88482551188fd1b9a74bb1f44206
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-05-31 10:01:05 +0300

    Add account management functionality with transaction methods and settings

--- js/lesson8.js
+++ js/lesson8.js
@@ -243,13 +243,165 @@
 // console.log(allTemps);
 
 //*Створення об'єктів
-const objA = (x: 1, y: 2);
-const objB = (x: 0, q: 3);
-const objC = {
-  ...objA, // x: 1, y: 2
-  x:10, // x: 10, y: 2
-  ...objB,// x: 0, y: 2, q:3
-  y:20 //  x: 0, y: 20, q:3
-  y:20
-}
-console.log(objC);// {x: 0, y: 20, q:3}
+// const objA = (x: 1, y: 2);
+// const objB = (x: 0, q: 3);
+// const objC = {
+//   ...objA, // x: 1, y: 2
+//   x:10, // x: 10, y: 2
+//   ...objB,// x: 0, y: 2, q:3
+//   y:20 //  x: 0, y: 20, q:3
+//   y:20
+// }
+// console.log(objC);// {x: 0, y: 20, q:3}
+
+// const defaultSettengs = {
+//   theme: "light",
+//   showoNotification: true,
+//   hideSidebar: false,
+// }
+
+// const userName = { 
+//   showoNotification: false,
+//   hideSidebar: true,
+// }
+
+// const settings = {
+//   ...defaultSettengs,
+//   ...userName,
+// }
+
+/*
+ * Типів транзакцій всього два.
+ * Можна покласти чи зняти гроші з рахунку.
+ */
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+
+
+
+
+/*
+ * Кожна транзакція це об'єкт із властивостями: id, type та amount
+ */
+
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+
+  // Історія транзакцій
+  transactions: [],
+
+
+
+  /*
+   * Метод створює та повертає об'єкт транзакції.
+   * Приймає суму та тип транзакції.
+   */
+  createTransaction(amount, type) {
+    const obj = { id: amount, amount, type };
+    return obj;
+  },
+
+
+
+
+
+  /*
+   * Метод, що відповідає за додавання суми до балансу.
+   * Приймає суму транзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його до історії транзакцій
+   */ 
+  deposit(amount) {
+    this.balance += amount;
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(transaction);
+  },
+
+
+
+
+
+  /*
+   * Метод, що відповідає за зняття суми з балансу.
+   * Приймає суму транзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його до історії транзакцій.
+   *
+   * Якщо amount більше ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливе, недостатньо коштів.
+   */
+  withdraw(amount) {
+    if(this.balance < amount) {
+      console.log("Недостатньок коштів");
+      return;
+    }
+
+    this.balance -= amount;
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transaction);
+  },
+
+
+
+
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() {
+    return this.balance;
+  },
+
+
+
+
+
+  /*
+   * Метод шукає та повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) {
+    // const this = account
+    for(const item of this.transactions) {
+      if(item.id === id) {
+        return item;
+      }
+    }
+    return "Not found";
+  },
+
+
+
+
+
+  /*
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) {
+    let sum = 0;
+    for(const item of this.transactions) {
+      if(item.type === type) {
+        sum += item.amount;
+      }
+    }
+
+    return sum;
+  },
+};
+
+  
+account.deposit(200);
+account.deposit(400);
+
+account.withdraw(100);
+
+// console.log(account.getTransactionDetails(100))
+
+// console.log(account.getBalance());
+// console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+
+// console.log(account);
