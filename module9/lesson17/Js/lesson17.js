86b9a622da401ff0b1106b354e69fe4953628ab9
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-07-02 07:30:53 +0300

    Refactor lesson scripts and HTML structure
    - Updated lesson3.js to improve comments and code clarity regarding conditional statements and scope.
    - Enhanced lesson11.js with clearer explanations of function execution context and the use of 'this'.
    - Modified index.html in module9 to streamline the structure and link to the correct JavaScript file.
    - Added lesson17.js with LocalStorage examples and removed unnecessary CSS files for a cleaner project.
    - Corrected script source references in index.html for lesson17.

--- module9/lesson17/Js/lesson17.js
+++ module9/lesson17/Js/lesson17.js
@@ -1,1 +1,54 @@
+// const LS_KEY = "Array of names";
+// const names = [' Alice', `Kate`, "Emma"];
+// const link = document.querySelector("a");
+// link.addEventListener("click",(event) =>  )
 
+//*LocalStorage
+// Збереження
+// Чому треба використовувати метод JSON.stringify*/
+// console.log(window);
+// localStorage.setItem(LS_KEY, JSON.stringify(names)); //
+
+//* Читання
+// //* Чому треба використовувати метод JSON.parse/
+
+// const value = localStorage.getItem(LS_KEY);
+// console.log(velue);
+
+// //* Видалення */
+// //localStorage.removeItem(LS_KEY)
+// localStorage.clear(); //
+
+//* localStorage не може зберігати функції*/
+// function add(a, bparams) {
+//   return a + b;
+
+// }
+
+// localStorage.setItem("foo", add);
+// const foo = JSON.parse(localStorage.getItem("foo"));
+// console.log(foo);
+
+// const calc = {
+//   a: 5,
+//   b: 10,
+//   add() {
+//     return this.a + this.b;
+//   },
+// };
+
+// localStorage.setItem("foo", JSON.stringify(calc));
+// const velue = localStorage.getItem("foo");
+// console.log(JSON.parse(velue));
+
+// const STORAGE_KEY = "feedback-msg";
+// const form = document;
+
+//* Отримання значення зі сховища
+//* Якщо там щось було, оновлюємо DOM */
+
+// function populateTextarea() {
+//   const savedMassege = localStorage.getItem(STORAGE_KEY);
+//   if (savedMassege) {
+//   }
+// }
