092381462d781632aedb7fb2e20a0b47e3b197de
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-07-10 14:16:02 +0300

    Add notification and timer functionality; implement initial Timer class structure and notification display

--- module10/lesson19/Js/04-date.js
+++ module10/lesson19/Js/04-date.js
@@ -1,18 +1,56 @@
-const date = new Date("March 16, 2030 14:25:00");
-console.log("Date: ", date);
+//** Створення
+//* Unix час
+//* Методи
+//* Різниця часу
+//* Date.now   */
 
-console.log("Day: ", date.getDate());
+// const currentDate = new Date(); //* коли ми створюємо новий екземпляр класу то отримуємо обєкт, проте зараз ми бачимо не зовсім обєкт а його метод String
+// console.dir(currentDate); //* Thu Jul 10 2025 13:15:38 GMT+0300 (за східноєвропейським літнім часом)
 
-console.log("Day of the week: ", date.getDay());
+// const day = currentDate.getDay();
+// console.log(day); //* 4 повертає день тижня починаючи з нуля і з неділі (неділя -0, понеділок-1, вівторок-2, середа -3, четверг -4, і т.д)
 
-console.log("Month: ", date.getMonth());
+// const date = currentDate.getDate();
+// console.log(date); //* 10  повертає  яке сьогодні число місяця
 
-console.log("Full year: ", date.getFullYear());
+// const month = currentDate.getMonth();
+// console.log(month); //* 6 повертає поточний місяць починаючи  нуля і з січня (січень -0б .., липень-6)
 
-console.log("Hours: ", date.getHours());
+// const year = currentDate.getFullYear();
+// console.log(year);//* 2025
 
-console.log("Minutes: ", date.getMinutes());
+// currentDate.setFullYear(2007);//* перезаписуємо дату, зокрема рік
+// console.log(currentDate); //* Tue Jul 10 2007 13:39:10 GMT+0300 (за східноєвропейським літнім часом)
 
-console.log("Seconds: ", date.getSeconds());
+//! Метод Date.now */
 
-console.log("Milliseconds: ", date.getMilliseconds());
+// console.dir(currentDate.getTime()); //* 1752144690239 повертає кількісь мілісекунд яка прйшла з моменту 01.01.1970р до моменту виклику методу.
+
+// const currentDate = new Date("2007-07-02");
+// console.dir(currentDate); //* Mon Jul 02 2007 03:00:00 GMT+0300 (за східноєвропейським літнім часом)
+
+// const currentDate = new Date(0);
+// console.dir(currentDate);//* Thu Jan 01 1970 03:00:00 GMT+0300 (за східноєвропейським стандартним часом)
+// const currentDate = new Date(175855254555);
+// console.dir(currentDate); //* Tue Jul 29 1975 11:40:54 GMT+0300 (за східноєвропейським стандартним часом)
+
+// console.log(Date.now()); //* 1752144690239 повертає кількісь мілісекунд яка прйшла з моменту 01.01.1970р до моменту виклику методу.
+
+//* Різниця часу
+const startTime = Date.now();
+setTimeout(() => {
+  const currentTime = Date.now();
+  console.log(currentTime - startTime); //* час через який виконається код
+}, 2000);
+
+// console.log("Month: ", date.getMonth());
+
+// console.log("Full year: ", date.getFullYear());
+
+// console.log("Hours: ", date.getHours());
+
+// console.log("Minutes: ", date.getMinutes());
+
+// console.log("Seconds: ", date.getSeconds());
+
+// console.log("Milliseconds: ", date.getMilliseconds());
--- module10/lesson19/Js/05-timer.js
+++ module10/lesson19/Js/05-timer.js
@@ -3,67 +3,11 @@
  * запускати та зупиняти відлік часу
  */
 
-const startBtn = document.querySelector("button[data-action-start]");
-const stopBtn = document.querySelector("button[data-action-stop]");
-const clockface = document.querySelector(".clockface");
+const startBtn = document.querySelector("button[data-action-start]"); //* отримуємо елемент з сторінки для взаємодії кнопка старт
+const stopBtn = document.querySelector("button[data-action-stop]"); //* отримуємо елемент з сторінки для взаємодії кнопка стоп
+const clockface = document.querySelector(".clockface"); //* отримуємо елемент куди будемо відображати свій час
 
-class Timer {
-  constructor({ onTick }) {
-    this.onTick = onTick;
-    this.isActive = false;
-    this.intervalId = null;
+class Timer {}
 
-    this.init();
-  }
-
-  init() {
-    const time = this.getTimeComponent(0);
-    this.onTick(time);
-  }
-
-  start() {
-    if (this.isActive) {
-      return;
-    }
-
-    this.isActive = true;
-    const startTime = Date.now();
-
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-
-      const time = this.getTimeComponent(deltaTime);
-      this.onTick(time);
-    }, 1000);
-  }
-
-  stop() {
-    clearInterval(this.intervalId);
-    this.init();
-    this.isActive = false;
-  }
-
-  getTimeComponent(time) {
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    );
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
-    return { hours, mins, secs };
-  }
-
-  pad(value) {
-    return String(value).padStart(2, "0");
-  }
-}
-
-const time = new Timer({ onTick: updateClockface });
-
-startBtn.addEventListener("click", time.start.bind(time));
-stopBtn.addEventListener("click", time.stop.bind(time));
-
-function updateClockface({ hours, mins, secs }) {
-  clockface.textContent = `${hours}:${mins}:${secs}`;
-}
+const time = new Timer();
+console.log(time);
