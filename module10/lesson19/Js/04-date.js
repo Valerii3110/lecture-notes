3a20ae70f7d682263c0821df817c9891a8a03112
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-07-10 11:29:22 +0300

    Add alert styles, remove unused CSS files, and update index with navigation links; implement promise handling in lesson20.js

---
+++ module10/lesson19/Js/04-date.js
@@ -0,0 +1,18 @@
+const date = new Date("March 16, 2030 14:25:00");
+console.log("Date: ", date);
+
+console.log("Day: ", date.getDate());
+
+console.log("Day of the week: ", date.getDay());
+
+console.log("Month: ", date.getMonth());
+
+console.log("Full year: ", date.getFullYear());
+
+console.log("Hours: ", date.getHours());
+
+console.log("Minutes: ", date.getMinutes());
+
+console.log("Seconds: ", date.getSeconds());
+
+console.log("Milliseconds: ", date.getMilliseconds());
