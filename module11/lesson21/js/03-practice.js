84f8dc020329f7b6b6d3705f6dfa3b23bb18727f
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-07-19 12:59:48 +0300

    remove lesson21 and lesson22 files; update 03-practice.html and index.html for improved functionality and styling

--- module11/lesson21/js/03-practice.js
+++ module11/lesson21/js/03-practice.js
@@ -1,81 +1,90 @@
-// Потрібно створити функціонал для отримання прогнозу погоди в місті.
-// Використай публічне API https://www.weatherapi.com/docs/
-// Використовуй ендпоінт Forecast для того, щоб отримати прогноз погоди на декілька днів.
+import axios from 'axios';
 
-// Створи форму в яку користувач:
-// 1 вводить назву міста.
-// 2 обирає на яку кількість днів він хоче отримати прогноз погоди (3, 5 та 7 днів).
-// (Іноді параметр не працює в такому випадку можна зробити пошук на 1, 2 та 3 дні)
-// Приклад форми https://prnt.sc/kFmLOj6gBdv-
+import '../common.css';
 
-// Після сабміту форми відмалюй картки з інформацією отриманою з бекенду.
-// Картка має містити відомості про:
-// 1 Зображення з погодою (icon)
-// 2 Текст з погодою (text)
-// 3 Дату (date)
-// 4 Середню температуру в Цельсія (avgtemp_c)
-// Приклад картки https://prnt.sc/h_p-A6Hty-i-
+/**
+ * https://thecatapi.com/
+ *
+ * 1) Створити сторінку яка при завантаженні робить запит на бекенд і оримує
+ *  всі доступні breeds. https://api.thecatapi.com/v1/breeds
+ *
+ * 2) В інтерфейсі є форма пошуку з прив'язаним datalist з завантаженими breeds.
+ *
+ * 3) Користувач в формі вводить або обирає необхідний breed і при сабміті форми
+ *  виконується GET запит за зображенням.
+ *
+ * 4) Після запиту під формою відображається картка з зображенням.
+ *
+ * 5) Під час запиту під формую відображається loader
+ */
 
-const BASE_URL = "http://api.weatherapi.com/v1";
-const API_KEY = "6410346f89264d6e919165208231505";
+const form = document.querySelector('#search-form');
+const breedInput = document.querySelector('#breed-input');
+const breedsList = document.querySelector('#breeds-list');
+const loader = document.querySelector('#loader');
+const catCard = document.querySelector('#cat-card');
 
-const form = document.querySelector(".js-search-form");
-const list = document.querySelector(".js-list");
+form.addEventListener('submit', handleSubmit);
 
-form.addEventListener("submit", handleSubmit);
+fetchBreeds();
+
+function fetchBreeds() {
+  axios('https://api.thecatapi.com/v1/breeds')
+    .then((res) => {
+      breedsList.insertAdjacentHTML('beforeend', populateDatalist(res.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function populateDatalist(arr) {
+  return arr
+    .map(
+      ({ id, name }) => `
+        <option value="${name}" data-id="${id}"></option>
+    `
+    )
+    .join('');
+}
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const { city, days } = event.currentTarget.elements;
+  const myBreed = breedInput.value;
+  const selectedBreed = Array.from(breedsList.options).find(
+    (item) => item.value.toLowerCase() === myBreed.toLowerCase()
+  );
 
-  serviceWeather(city.value, days.value)
-    .then((data) => {
-      console.log(data);
-      list.innerHTML = createMarkup(data.forecast.forecastday);
+  if (!selectedBreed) {
+    alert('Оберіть існуючу породу');
+    return;
+  }
+
+  const breedId = selectedBreed.dataset.id;
+  loader.classList.remove('hidden');
+
+  axios(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((res) => {
+      if (res.data.length > 0) {
+        const catImg = res.data[0].url;
+
+        catCard.innerHTML = `
+                    <div class="card">
+                        <img src="${catImg}" alt="${myBreed}" class="card-image"/>
+                        <div class="card-body">
+                            <h2 class="card-title">${myBreed}</h2>
+                        </div>
+                    </div>
+                `;
+      } else {
+        catCard.innerHTML = `<p class="error-text">Зображення не знайдено</p>`;
+      }
     })
     .catch((error) => {
-      list.innerHTML = `<h3>${error.message}</h3>`;
+      console.log(error);
     })
     .finally(() => {
-      event.target.reset();
+      loader.classList.add('hidden');
     });
-}
-
-function serviceWeather(city = "", days = 1) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: city,
-    days: days,
-    lang: "uk",
-  });
-
-  return fetch(`${BASE_URL}/forecast.json?${params}`).then((result) => {
-    if (!result.ok) {
-      throw new Error(result.statusText);
-    }
-
-    return result.json();
-  });
-}
-
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({
-        date,
-        day: {
-          avgtemp_c,
-          condition: { icon, text },
-        },
-      }) => `
-        <li class="weather-card">
-            <img src="${icon}" alt="${text}" class="weather-icon"/>
-            <h2 class="weather-date">${date}</h2>
-            <h3 class="weather-text">${text}</h3>
-            <h3 class="temperature">${avgtemp_c} °C</h3>
-        </li>
-    `
-    )
-    .join("");
 }
